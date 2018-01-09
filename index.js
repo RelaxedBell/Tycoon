0c714890579310568d7457fe2e153af78850d5f3
Lukey <lukemueller@charter.net>
2018-01-09 15:13:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  io.emit('chat message', 'User connected');
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    io.emit('chat message', 'User disconnected');
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
